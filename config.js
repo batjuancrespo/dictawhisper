10584b6ef54c78075d3df8969e6bbf73bbdc2e6c
batjuancrespo <batjuancrespo@gmail.com>
2025-03-26 19:04:00 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_VQH1y-px8-QF3gMw3VOPjiiU1OefDBo",
+  authDomain: "almacena-correcciones-dictado.firebaseapp.com", 
+  projectId: "almacena-correcciones-dictado",
+  storageBucket: "almacena-correcciones-dictado.appspot.com",
+  messagingSenderId: "209194920272",
+  appId: "1:209194920272:web:ccbec69d0a5aa88789e455",
+  measurementId: "G-6PQSKYMDP0"
+};
+
+// User ID management (simple approach - could be enhanced with authentication)
+let userId = localStorage.getItem('userId');
+if (!userId) {
+  userId = 'user_' + Date.now() + '_' + Math.random().toString(36).substring(2, 9);
+  localStorage.setItem('userId', userId);
+  console.log("Created new user ID:", userId);
+} else {
+  console.log("Using existing user ID:", userId);
+}
+
+export { userId };
